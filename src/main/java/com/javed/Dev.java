dd118f0bfbee89a49fb2db8f2c1bc36986fd8dbc
Javed <javedali.bscsf22@iba-suk.edu.pk>
2025-02-19 13:11:33 +0500

    Uptill now, we did this in previous repo 'spring_level_0' through springboot, but here we did same with spring creating our own spring.xml configuration file. Next we'll see how dependencies work in spring, if Dev class depends on Laptop class

--- src/main/java/com/javed/Dev.java
+++ src/main/java/com/javed/Dev.java
@@ -1,6 +1,9 @@
 package com.javed;
 
 public class Dev {
+    public Dev(){
+        System.out.println("Dev construct successfully!");
+    }
 
     public void build() {
         System.out.println("project built successfully!");
